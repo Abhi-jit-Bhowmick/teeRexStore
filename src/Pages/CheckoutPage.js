6acaf146d9e5407abc64fbc316d88081acd429c1
Abhi-jit-Bhowmick <bumba.abhi2012@gmail.com>
2023-09-28 17:13:15 +0530

    Checking

--- src/Pages/CheckoutPage.js
+++ src/Pages/CheckoutPage.js
@@ -87,7 +87,7 @@ function CheckoutPage() {
               <div className="empty-cart-emoji">
                 <BsCartXFill />
               </div>
-              <div className="empty-cart-text">Your Cart is Empty .....</div>
+              <div className="empty-cart-text">Your Cart is Empty .....Abhi</div>
             </div>
           )}
         </div>
